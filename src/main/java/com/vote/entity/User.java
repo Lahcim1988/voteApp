775884290c9259b5dd74bf92dc2836715cd59706
Lahcim1988 <michalurbanek@op.pl>
2020-01-13 18:59:59 +0100

    Added ID and connect with DB

--- src/main/java/com/vote/entity/User.java
+++ src/main/java/com/vote/entity/User.java
@@ -1,12 +1,13 @@
 package com.vote.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")          // the table name in MySql
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;

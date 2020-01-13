fcd9c7bdf5c3f71b5e4b0683155211d6d9fc11ef
Lahcim1988 <michalurbanek@op.pl>
2020-01-13 19:44:16 +0100

    Many to One relation, Comment, Vote Class

---
+++ src/main/java/com/vote/entity/ComentId.java
@@ -0,0 +1,30 @@
+package com.vote.entity;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Embeddable
+public class ComentId implements Serializable {
+
+    private User user;
+    private Feature feature;
+
+    @ManyToOne
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @ManyToOne
+    public Feature getFeature() {
+        return feature;
+    }
+
+    public void setFeature(Feature feature) {
+        this.feature = feature;
+    }
+}

ff0139fcb64c00d29d5918ab2cda61abc46c5e2f
Lahcim1988 <michalurbanek@op.pl>
2020-01-18 12:57:52 +0100

    ProductController Save - Add switch button to "is published"

--- src/main/java/com/vote/controller/ProductController.java
+++ src/main/java/com/vote/controller/ProductController.java
@@ -4,12 +4,16 @@ import com.vote.entity.Product;
 import com.vote.entity.User;
 import com.vote.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @Controller
 public class ProductController {
@@ -23,8 +27,26 @@ public class ProductController {
     }
 
     @GetMapping("/products/{productId}")
-    public String getProduct(@PathVariable Long productId){
+    public String getProduct(@PathVariable Long productId, ModelMap model, HttpServletResponse response) throws Exception{
+        Optional<Product> productOpt = productRepo.findById(productId);
+
+        if(productOpt.isPresent()){
+            Product product = productOpt.get();
+            model.put("product", product);
+        }else {
+            Product product = new Product();
+            response.sendError(HttpStatus.NOT_FOUND.value(), "Product with id " + productId + " was not found");
+            return "product";
+        }
+
         return "product";
+    }
+
+    @PostMapping("/products/{productId}")                       // @PathVariable productId has to map with {productId}
+    public String saveProduct(@PathVariable Long productId, Product product){  // Binding data from product.html - save product <form action="" method="post>
+                                                                                // saving - product sa have to pass product to method {product.html --> product}
+        System.out.println(product);
+        return "redirect:/products/" + product.getId();                     // posting has to be redirect
     }
 
     @PostMapping("/products")                       // once we click CreateProduct is going to post and then is redirected to get
@@ -36,7 +58,7 @@ public class ProductController {
 
         product = productRepo.save(product);          // return Product
 
-        
+
 
         return "redirect:/products/" + product.getId();                // redirect always with product - go to specific product
     }
